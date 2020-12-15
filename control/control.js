aafab553d182673b7927f46d53c86775b2358aa4
HassenBenAbid <hassenbenabid@protonmail.com>
2020-12-15 16:26:56 +0100

    update

--- control/control.js
+++ control/control.js
@@ -1,88 +1,41 @@
-/*function smoothScroll(target, duration){
-	var target = document.querySelector(target);
-	var targetPos = target.getBoundingClientRect().top;
-	var CurrentPos = window.pageYOffset;
-	var distance = targetPos - CurrentPos;
-	var startTime = null ;
 
-	console.log(CurrentPos);
-}
+var main = (function(){
+	
+	$('#headerHolder').load('headerPre.html', () => {
+		
+		$(function() {
 
-function animation()
+			$('a[href^=\\#]').click(function() {
 
-function ease(t, b, c, d) {
-	return c * Math.sin(t/d * (Math.PI/2)) + b;
-};
+				if (window.location.pathname.indexOf('index') == -1)
+					window.location.pathname = "index.html";
+		
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+				  $('html,body').animate({
+					scrollTop: target.offset().top
+				  }, 500);
+				}
+			});
 
-window.requestAnimationFrame(animation);
+			if(window.location.hash){
+				scroll(0, 0);
+				setTimeout(function(){
+					scroll(0, 0);
+				}, 1);
+			}
 
-smoothScroll('.contactText', 1000);*/
+			if (window.location.hash){
+				$('html,body').animate({
+					scrollTop: $(window.location.hash).offset().top + 'px'
+				  }, 500);
+			}
 
-function smoothScroll(target, duration) {
-
-	var target = document.querySelector(target);
-	var targetPos = target.getBoundingClientRect().top;
-	var CurrentPos = window.pageYOffset;
-	var startTime = null ;
-
-	function animation(currentTime){
-		if (!startTime) startTime = currentTime ;
-		var timeElapsed = currentTime - startTime;
-		var run = ease(timeElapsed, CurrentPos, targetPos, duration);
-		window.scrollTo(0, run);
-		if (timeElapsed < duration) window.requestAnimationFrame(animation);
-	}
-
-	function ease(t, b, c, d) {
-	return c * Math.sin(t/d * (Math.PI/2)) + b;
-	};
-
-	window.requestAnimationFrame(animation);
-
-};
-
-var navEvent = (function(){
-
-	var t = 1500 ;
-
-	document.querySelector('#about').addEventListener('click', function (){
-		smoothScroll('#aboutC',t);
-	});
-	document.getElementById('Professional').addEventListener('click', function(){
-		smoothScroll('#profC',t);
-	});
-		document.getElementById('personal').addEventListener('click', function(){
-		smoothScroll('#persC',t);
-	});
-	document.getElementById('contact').addEventListener('click', function(){
-		smoothScroll('#contactC',t);
-	});
-	document.querySelector('.home').addEventListener('click', function(){
-		smoothScroll('.IntroSec',t);
-	});
-
-	document.getElementById('ceosImg').addEventListener('mouseover', function(){
-		hoverImg('#ceosImg', './images/CeosPhone.PNG')
-	});
-
-	document.getElementById('ceosImg').addEventListener('mouseout', function(){
-		hoverImg('#ceosImg', './images/CeosDisplay.PNG')
-	});
+		  });
+	})
 
 })();
-
-
-function hoverImg(element, imageLocation){
-	var target = document.querySelector(element);
-	target.classList.add("parallaxOut");
-
-	window.setTimeout(() => {
-		target.setAttribute('src', imageLocation);
-		target.classList.remove("parallaxOut");
-		target.classList.add("parallax");
-	}, 300);
-	
-}
 
 
 
